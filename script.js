ffb87621e6bf2e8a5c194d82b4f711184bae398e
Faruk <97340877+Faruk-Tutkus@users.noreply.github.com>
2023-04-01 16:22:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const info = document.getElementById("info");
 //Hover
 challenge.addEventListener("mouseover", function(){info.textContent = "Challenge"});
 challenge.addEventListener("mouseleave", function() {info.textContent = ""});
-trainer.addEventListener("mouseover", function(){info.textContent = "Trainer"});
+trainer.addEventListener("mouseover", function(){info.textContent = "Tracking"});
 trainer.addEventListener("mouseleave", function() {info.textContent = ""});
 //Click
 challenge.addEventListener("click", function(){});
